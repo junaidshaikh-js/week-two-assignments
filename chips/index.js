3d204623f48dc4ab3eae060a1172cef2f505c953
Junaid <sj.shaikhjunaid@gmail.com>
2022-01-20 15:03:03 +0530

    implement the functionality

--- chips/index.js
+++ chips/index.js
@@ -1,1 +1,49 @@
+const fruitInput = document.querySelector("#fav-fruit");
+const chipContainer = document.querySelector(".chip-container");
+const chipWrappers = document.querySelectorAll(".chip-wrapper");
 
+fruitInput.addEventListener("change", addChip);
+
+chipWrappers.forEach((chipWrapper) => {
+  chipWrapper.addEventListener("click", removeChip);
+});
+
+// ********
+
+function addChip(e) {
+  const value = e.target.value;
+
+  const chip = makeChip(value);
+
+  chipContainer.insertBefore(
+    chip,
+    chipContainer.children[chipContainer.children.length - 1]
+  );
+
+  fruitInput.value = "";
+}
+
+function makeChip(value) {
+  const chipWrapper = document.createElement("div");
+
+  chipWrapper.className = "chip-wrapper";
+
+  chipWrapper.innerHTML = `
+        <div class="chip">${value}</div>
+        <span class="close">
+            <i class="fas fa-times"></i>
+        </span>
+    `;
+
+  chipWrapper.addEventListener("click", removeChip);
+
+  return chipWrapper;
+}
+
+function removeChip(e) {
+  let tagName = e.target.tagName;
+
+  if (tagName == "I" || tagName == "SPAN") {
+    e.currentTarget.remove();
+  }
+}
